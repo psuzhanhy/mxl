bc5ec5832a5910727b5a03ea7cb9cc73642131fe
hongyuanzhan <hongyuan.zhan@gmail.com>
2018-03-28 00:03:55 -0400

    test for SGD and timing

--- testing/testgmxl.cpp
+++ testing/testgmxl.cpp
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
     ReadDenseInput(input_filename, &data);   
 	CSR_matrix xf = Dense2CSR(data);
 	MxlGaussianBlockDiag gmxl1 = MxlGaussianBlockDiag(xf, data.label,
-						 numclass, xf.number_cols, false, 1000);
+						 numclass, xf.number_cols, true, 1000);
 
 	/*
 	MxlGaussianBlockDiag gmxl2 = MxlGaussianBlockDiag(xf, data.label,
@@ -42,14 +42,25 @@ int main(int argc, char **argv)
 	
 	//for(auto val : prob)
 	//	std::cout << val << std::endl;
+	
+	struct timeval start, finish;
+    gettimeofday(&start,nullptr) ; // set timer start      
+	std::cout << "Initial NLL: " << gmxl1.negativeLogLik() << std::endl;
+    gettimeofday(&finish,nullptr) ; // set timer finish      
+	double elapsedTime = finish.tv_sec - start.tv_sec;      // sec 
+    elapsedTime += (finish.tv_usec - start.tv_usec)/(1000.0 * 1000.0);   // us to sec
+	std::cout << "time for computing NLL: " << elapsedTime << std::endl;
 		
-	std::cout << "NLL: " << gmxl1.negativeLogLik() << std::endl;
-	//std::cout << "NLL: " << gmxl2.negativeLogLik() << std::endl;
-
-
-
-
-
+	double stepsize = 1.0;
+	double scalar = 1.0;
+	int maxEpochs = 1;
+    gettimeofday(&start,nullptr) ; // set timer start      
+	gmxl1.fit(stepsize,scalar,maxEpochs);
+    gettimeofday(&finish,nullptr) ; // set timer finish      
+	elapsedTime = finish.tv_sec - start.tv_sec;      // sec 
+    elapsedTime += (finish.tv_usec - start.tv_usec)/(1000.0 * 1000.0);   // us to sec
+	std::cout << "time for one SGD epoch : " << elapsedTime << std::endl;
+	std::cout << "NLL after " << maxEpochs << " epochs: "  << gmxl1.negativeLogLik() << std::endl;
 
 
 	return 0;
