888ccc9d38a4b01022348071a5bac5bf7ea54e1b
hongyuanzhan <hongyuan.zhan@gmail.com>
2018-03-30 02:21:44 -0400

    replace boost by Random123

--- src/param/matvec.h
+++ src/param/matvec.h
@@ -3,9 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
-#include <boost/random.hpp>
-#include <boost/random/normal_distribution.hpp>
-#include <boost/generator_iterator.hpp>
 
 struct CSR_matrix  
 {
