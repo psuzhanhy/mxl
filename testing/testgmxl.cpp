c19dd685edf8b8933e6cbd58ab08554d1bd478b5
hongyuanzhan <hongyuan.zhan@gmail.com>
2018-04-03 01:59:09 -0400

    test hybrid SGD + AGD method

--- testing/testgmxl.cpp
+++ testing/testgmxl.cpp
@@ -7,6 +7,7 @@
 #include "matvec.h"
 #include "mxl_gaussblockdiag.h"
 #include "readinput.h"
+#include "opthistory.h"
 
 int main(int argc, char **argv) 
 {
@@ -19,50 +20,20 @@ int main(int argc, char **argv)
     ReadDenseInput(input_filename, &data);   
 	CSR_matrix xf = Dense2CSR(data);
 	MxlGaussianBlockDiag gmxl1 = MxlGaussianBlockDiag(xf, data.label,
-						 numclass, xf.number_cols, false, 1000);
-
-	/*
-	MxlGaussianBlockDiag gmxl2 = MxlGaussianBlockDiag(xf, data.label,
 						 numclass, xf.number_cols, false, 100);
 
-	auto choleskey1 = gmxl1.getCovCholeskey();
-	auto  choleskey2 = gmxl2.getCovCholeskey();
-	
-	if (choleskey1 == choleskey2)
-	{
-		std::cout << "same \n";
-	} else {
-		std::cout << "not same \n";	
-	}
-	*/	
-
-	//auto prob = gmxl1.simulatedProbability(0);
-	
-	//for(auto val : prob)
-	//	std::cout << val << std::endl;
 		
-	struct timeval start, finish;
-    
-	//gettimeofday(&start,nullptr) ; // set timer start      
-	//std::cout << "Initial NLL: " << gmxl1.negativeLogLik() << std::endl;
-   // gettimeofday(&finish,nullptr) ; // set timer finish      
-	double elapsedTime; 
-	//elapsedTime = finish.tv_sec - start.tv_sec;      // sec 
-    //elapsedTime += (finish.tv_usec - start.tv_usec)/(1000.0 * 1000.0);   // us to sec
-	//std::cout << "time for computing NLL: " << elapsedTime << std::endl;
-		
-	double stepsize = 1.0;
+	double stepsize_SGD = 1.0 * xf.number_rows;
+	double stepsize_APG = 0.01 * xf.number_rows;
 	double scalar = 1.0;
-	int maxEpochs = 1;
-    gettimeofday(&start,nullptr) ; // set timer start      
-	gmxl1.fit(stepsize,scalar,maxEpochs);
-    gettimeofday(&finish,nullptr) ; // set timer finish      
-	elapsedTime = finish.tv_sec - start.tv_sec;      // sec 
-    elapsedTime += (finish.tv_usec - start.tv_usec)/(1000.0 * 1000.0);   // us to sec
-	std::cout << "time for one SGD epoch : " << elapsedTime << std::endl;
-	std::cout << "NLL after " << maxEpochs << " epochs: "  << gmxl1.negativeLogLik() << std::endl;
-
-
+	double momentum = 1.0;
+	double shrinkage = 0.9;
+	int maxEpochs = 10;
+   
+	OptHistory sgdHistory(maxEpochs), apgHistory(maxEpochs); 
+	gmxl1.fit_by_SGD(stepsize_SGD,scalar,maxEpochs, sgdHistory);
+	gmxl1.fit_by_APG(stepsize_APG, momentum, shrinkage, maxEpochs, apgHistory);	
+    
 	return 0;
 }
 
